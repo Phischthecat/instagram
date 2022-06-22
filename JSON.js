34186402d8913eb36b7d1c22b86581ef0e173077
Phil Schmucker <p.schmuck.er@web.de>
2022-06-22 21:43:54 +0200

    ready

--- JSON.js
+++ JSON.js
@@ -1,26 +1,70 @@
 let content = [
   {
-    author: 'Tagesschau',
-    images: 'img/img1.png',
-    location: 'London, United Kingdom',
-    headline: 'lorem',
+    author: 'Apotheken Wegschau',
+    images: 'img/colin-maynard-CEEhmAGpYzE-unsplash.jpg',
+    logo: 'img/logos/apotheke.png',
+    location: 'Berlin, Germany',
+    headline:
+      'Die Geburtenrate steigt! Dieser gesellschaftliche Vorteil von Corona kommt erst 9 Monate später zum Vorschein. Lesen Sie hier die beliebtesten Kindernamen 2022',
+    likes: 1782,
+    comment: [],
   },
   {
-    author: 'Tagesschau',
-    images: 'img/img2.png',
-    location: 'Mexiko Stadt, Mexiko',
-    headline: 'lorem',
+    author: 'Urlaubsgauner.de',
+    images: 'img/m-ashraful-alam-Sbl4igfr53E-unsplash.jpg',
+    logo: 'img/logos/palmen.png',
+    location: 'Berlin, Germany',
+    headline:
+      'Alles wird teurer. Der Trend geht zu reisen mit dem Camper. Weg von dem Touri-Pfaden, direkt in der Natur',
+    likes: 2574,
+    comment: [],
   },
   {
-    author: 'Bild.de',
-    images: 'img/img3.png',
-    location: 'Berlin, Deutschland',
-    headline: 'lorem',
+    author: 'ReportReportReport.de',
+    images: 'img/niklas-hamann-R0rYa09bEx8-unsplash.jpg',
+    logo: 'img/logos/nachrichtensprecherin.png',
+    location: 'Berlin, Germany',
+    headline:
+      'Auch dieses Jahr schlägt wieder eine neue Hitzewelle über Deutschland. Am ersten Hochsommerwochenende klettern die Temperaturen auf über 37 Grad in Berlin.',
+    likes: 8593,
+    comment: [],
   },
   {
-    author: 'Welt',
-    images: 'img/img4.png',
-    location: 'Hongkong, China',
-    headline: 'lorem',
+    author: 'Fleisch, Feuer und Freunde',
+    images: 'img/photo_-reflect-CT3BGFLHzIM-unsplash.jpg',
+    logo: 'img/logos/wurstchen.png',
+    location: 'Berlin, Germany',
+    headline:
+      'Seit dem 1. März ist die Grillsaison gestartet und es wird viel experiementiert',
+    likes: 5439,
+    comment: [],
+  },
+];
+
+let suggestions = [
+  {
+    logo: './img/suggestions/corey-young--A5tWSJBzc8-unsplash.jpg',
+    name: 'Corey Young',
+    info: 'Neu auf Graminsta',
+  },
+  {
+    logo: './img/suggestions/julia-koi-dlQPPdiaN4g-unsplash.jpg',
+    name: 'Julia Koi',
+    info: 'Neu auf Graminsta',
+  },
+  {
+    logo: './img/suggestions/kajetan-sumila-yUczWZuVYyc-unsplash.jpg',
+    name: 'Kajetan Sumila',
+    info: 'Neu auf Graminsta',
+  },
+  {
+    logo: './img/suggestions/richard-cordones-n4uuNENctWk-unsplash.jpg',
+    name: 'Richard Cordones',
+    info: 'Neu auf Graminsta',
+  },
+  {
+    logo: './img/suggestions/vitaliy-rigalovsky-ZN4_AI8cJ2c-unsplash.jpg',
+    name: 'Vitaliy Rigalovsky',
+    info: 'Neu auf Graminsta',
   },
 ];
--- script.js
+++ script.js
@@ -1,15 +1,152 @@
-function show() {
+function render() {
+  generatePosts();
+  generateSugg();
+  generateStory();
+}
+
+function generatePosts() {
   document.getElementById('postcontainer').innerHMTL = '';
 
   for (let i = 0; i < content.length; i++) {
-    const post = content[i];
+    document.getElementById('postcontainer').innerHTML += createPost(i);
+  }
+}
 
-    document.getElementById('postcontainer').innerHTML += /*html*/ `
-        <div>
-            <img src="${post['images']}" alt="">
-            <div>${post['author']}</div>
-            <div>${post['headline']}</div>
+function generateSugg() {
+  document.getElementById('suggProfile').innerHTML = '';
+  for (let i = 0; i < suggestions.length; i++) {
+    document.getElementById('suggProfile').innerHTML += createSugg(i);
+  }
+}
+
+function generateStory() {
+  document.getElementById('storys').innerHTML = '';
+
+  for (let i = 0; i < content.length; i++) {
+    document.getElementById('storys').innerHTML += createStory(i);
+  }
+}
+
+function createPost(i) {
+  const post = content[i];
+
+  return /*html*/ `
+        <div id="post" class="post post-box">
+          <div class="header-post">
+            <div class="header-wrapper">
+              <div class="logoAndAuthor">
+                <img id="postLogo" class="logoAuthor pointer" src="${post['logo']}" alt="" />
+                <div class="authorLocation">
+                  <h2 class="pointer" id="postAuthor">${post['author']}</h2>
+                  <p id="postLocation">${post['location']}</p>
+                </div>
+              </div>
+              <img class="post-menu pointer" src="./img/icons/punkte.png" alt="" />
+            </div>
+          </div>
+          <img class="image" id="postImage" src="${post['images']}" alt="" />
+          <div class="interact">
+            <div class="interactIcons">
+              <img id="heartIcon${i}" class="icons pointer" src="./img/icons/herz.png" alt="herz" onclick="like(${i})"/>
+              <img class="icons pointer" src="./img/icons/chat-ballon.png" alt="chat" />
+              <img class="icons pointer" src="./img/icons/nachricht.png" alt="commentar" />
+            </div>
+            <img class="icons pointer" src="./img/icons/instagram-speichern.png" alt="save" />
+          </div>
+          <p class="likeCount">Gefällt <span id="likeAmount${i}">${post['likes']}</span> Mal</p>
+          <div class="postBody">
+            <p><strong>${post['author']}</strong> ${post['headline']}</p>
+          </div>
+
+          <div class="comment" id="comment${i}"></div>
+          <div id="commentInput">
+            <div>
+              <img class="pointer" src="./img/icons/smile.png" alt=""> 
+              <input id="input${i}" class="commentInput" type="text" placeholder="Kommentieren...">
+
+            </div>
+            <button class="pointer" onclick="addComment(${i})">Posten</button>
+          </div>
         </div>
         `;
+}
+
+function createSugg(i) {
+  const sugg = suggestions[i];
+
+  return /*html*/ `
+    <div class="container-suggestions">
+      <div class="suggProfile">
+        <img class="suggLogo pointer" src="${sugg['logo']}" alt="logo">
+        <div>
+          <h2 class="pointer">${sugg['name']}</h2>
+          <p>${sugg['info']}</p>
+        </div>
+      </div>
+      <button id="follow${i}" class="followBtn pointer follow" onclick="follow(${i})">Folgen</button>
+    </div>
+    `;
+}
+
+function createStory(i) {
+  const story = content[i];
+  return /*html*/ `
+  <div class="storyPost">
+  <img class="logo pointer" src="${story['logo']}" alt="" />
+  <h3 class="pointer">${story['author']}</h3>
+  </div>
+  `;
+}
+
+function addComment(i) {
+  let add = document.getElementById(`comment${i}`);
+  add.innerHTML = '';
+  let comment = document.getElementById(`input${i}`);
+  content[i]['comment'].push(comment.value);
+  for (let j = 0; j < content[i]['comment'].length; j++) {
+    add.innerHTML += /*html*/ `
+    <div class="comment-section">
+      <img class="logoComment pointer" src="./img/profile.jpg" alt="" />
+      <h3 class="pointer">KevinPhotographie_official</h3>
+      <p>${content[i]['comment'][j]}</p>      
+    </div> 
+    `;
   }
+  comment.value = '';
+}
+
+function like(i) {
+  let like = content[i]['likes'];
+  document.getElementById(`likeAmount${i}`).innerHTML = like + 1;
+  document.getElementById(`heartIcon${i}`).src = './img/icons/herz_rot.png';
+  document
+    .getElementById(`heartIcon${i}`)
+    .setAttribute('onclick', `javascript: dislike(${i})`);
+}
+
+function dislike(i) {
+  let like = content[i]['likes'];
+  document.getElementById(`likeAmount${i}`).innerHTML = like + 1 - 1;
+  document.getElementById(`heartIcon${i}`).src = './img/icons/herz.png';
+  document
+    .getElementById(`heartIcon${i}`)
+    .setAttribute('onclick', `javascript: like(${i})`);
+}
+
+function follow(i) {
+  document.getElementById(`follow${i}`).innerHTML = 'Entfolgen';
+  document.getElementById(`follow${i}`).classList.add('unfollow');
+  document.getElementById(`follow${i}`).classList.remove('follow');
+  document
+    .getElementById(`follow${i}`)
+    .setAttribute('onclick', `javascript: unfollow(${i})`);
+}
+
+function unfollow(i) {
+  document.getElementById(`follow${i}`).innerHTML = 'Folgen';
+  document.getElementById(`follow${i}`).classList.add('follow');
+  document.getElementById(`follow${i}`).classList.remove('unfollow');
+  document
+    .getElementById(`follow${i}`)
+    .setAttribute('onclick', `javascript: follow(${i})`);
 }
